6447d7f6dbb03e5b43fafee5551a039b5d8f6816
LIN Yizhou <yizhou.lin@outlook.com>
2020-01-19 09:02:26 +0800

    black list

---
+++ bin/getDown.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [[ -z $1 ]]; then
+    echo "kw needed"
+    exit
+fi
+
+tag=tieba_img
+if [[ !-z $2 ]]; then
+    tag=$2
+fi
+
+cd /home/ylin/attachementUtils/
+source env/bin/activate
+systemd-cat -t $tag python ./attachementUtils/attachementUtil.py -d sqlite:////home/ylin/anna/ext_hdd/by-uuid/aea3c7d1-7bf3-4028-a87f-b9dc140a6eec/all.$1.tieba.baidu.com.db -p /home/ylin/anna/ext_hdd/by-uuid/aea3c7d1-7bf3-4028-a87f-b9dc140a6eec/$1IMG/
